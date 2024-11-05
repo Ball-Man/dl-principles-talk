05c6d8fe4eb6beb08de6e07410ca59f9a54dfd06
Francesco Mistri <francesco.mistri@bi.mpg.de>
2024-11-05 11:39:52 +0100

    Add static welcome slide

--- slides.py
+++ slides.py
@@ -2,10 +2,19 @@ from manim import *
 from manim_slides.slide import Slide
 
 
-class ExampleText(Slide):
+def static_slide(slide, tiny_time=0.1):
+    """Apply a tiny wait animation to simulate a static slide."""
+    slide.play(Wait(tiny_time))
+
+
+class Welcome(Slide):
 
     def construct(self):
-        text = Tex('Hello everyone $2\pi$')
-        self.play(Write(text))
+        text = Text('Deep Learning Principles')
+        line = Line(2 * LEFT, 2 * RIGHT).next_to(text, DOWN)
+
+        self.add(text)
+        self.add(line)
+        static_slide(self)
 
         self.next_slide()
